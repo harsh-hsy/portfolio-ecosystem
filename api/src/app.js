69c340b7b8e75831251cb5a9555f0e9d9d687bbc
Harsh Singh <codewithharshsingh@gmail.com>
2026-07-30 17:49:42 +0530

    fix: handle API favicon request

--- api/src/app.js
+++ api/src/app.js
@@ -15,6 +15,10 @@ app.use(cors(corsOptions))
 app.use(express.json({ limit: '1mb' }))
 app.use(cookieParser())
 
+app.get('/favicon.ico', (req, res) => {
+  res.status(204).end()
+})
+
 app.get('/', (req, res) => {
   res.json({
     ok: true,
